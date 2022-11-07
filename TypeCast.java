67eb50571a5db23a945f8f56f3622d49744a10ef
Rakin Mohammad Sifullah <rakin.sifullah@gmail.com>
2022-11-07 23:17:49 +0600

    Type Cast

---
+++ TypeCast.java
@@ -0,0 +1,16 @@
+public class TypeCast {
+    public static void main(String args[]) {
+      double a = 50.50;
+      System.out.println("Double represents: "+a);
+      
+      float f = (float) a;
+      System.out.println("Float represents: "+f);
+      
+      long b = (long) a;
+      System.out.println("Float represents: "+b);
+      
+      int c = (int) b;
+      System.out.println("Int represents: "+c);
+      
+    }
+}
