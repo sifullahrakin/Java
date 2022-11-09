e99e28a53b3bc89fc192764ee63063262b855ce0
Rakin Mohammad Sifullah <rakin.sifullah@gmail.com>
2022-11-09 20:38:02 +0600

    Bitwise

---
+++ Operators/src/operators/Operator_Bitwise.java
@@ -0,0 +1,21 @@
+package operators;
+
+public class Operator_Bitwise {
+
+	public static void main(String[] args) {
+		
+		//Initial values
+		int a = 5;
+		int b = 7;
+		
+		//bitwise and
+		//0101 & 0111 = 0101 = 5
+		System.out.println("a & b = "+(a & b));
+		
+		//bitwise or
+		//0101 | 0111 = 0111 = 7
+		System.out.println("a | b = "+(a | b));
+
+	}
+
+}
