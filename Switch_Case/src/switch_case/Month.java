7f367708600b5ad8bfe3b893e03f5aae46a87d9f
Rakin Mohammad Sifullah <rakin.sifullah@gmail.com>
2022-11-11 19:50:47 +0600

    Switch Case

---
+++ Switch_Case/src/switch_case/Month.java
@@ -0,0 +1,58 @@
+package switch_case;
+import java.util.*;
+
+public class Month {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter Month Number: ");
+		int month = sc.nextInt();
+		
+		String monthString;
+		
+		switch(month) {
+		case 1: 
+			monthString = "January";
+			break;
+		case 2: 
+			monthString = "February";
+			break;
+		case 3: 
+			monthString = "March";
+			break;
+		case 4: 
+			monthString = "April";
+			break;
+		case 5: 
+			monthString = "May";
+			break;
+		case 6: 
+			monthString = "June";
+			break;
+		case 7: 
+			monthString = "July";
+			break;
+		case 8: 
+			monthString = "August";
+			break;
+		case 9: 
+			monthString = "September";
+			break;
+		case 10: 
+			monthString = "October";
+			break;
+		case 11: 
+			monthString = "November";
+			break;
+		case 12: 
+			monthString = "December";
+			break;
+		default:
+		    monthString = "unrecognized month";
+		}
+		System.out.println(monthString);
+	
+	}
+
+	}
