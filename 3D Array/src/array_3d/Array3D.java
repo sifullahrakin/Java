0f17fb90051526aa9f90caba09e0ef4f124bf9de
Rakin Mohammad Sifullah <rakin.sifullah@gmail.com>
2022-11-14 00:35:22 +0600

    3D Array

---
+++ 3D Array/src/array_3d/Array3D.java
@@ -0,0 +1,11 @@
+package array_3d;
+
+public class Array3D {
+
+	public static void main(String[] args) {
+		int arr[][][] = {{{1,2,3},{4,5,6}},{{11,12,13},{14,15,16}},{{21,22,23},{24,25,26}}};
+		System.out.println(arr[0][1][2]);
+
+	}
+
+}
