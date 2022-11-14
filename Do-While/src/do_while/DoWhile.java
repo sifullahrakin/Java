63b724ce17bb1a675bf4ab0c6557165e51d333c9
Rakin Mohammad Sifullah <rakin.sifullah@gmail.com>
2022-11-15 00:25:23 +0600

    Do While

---
+++ Do-While/src/do_while/DoWhile.java
@@ -0,0 +1,15 @@
+package do_while;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int count = 1;
+		
+		do {
+			System.out.println("Count is: "+count);
+			count++;
+		} while(count<10);
+
+	}
+
+}
---
+++ Do-While/src/do_while/While.java
@@ -0,0 +1,16 @@
+package do_while;
+
+public class While {
+
+	public static void main(String[] args) {
+		
+		int count = 1;
+		
+		while(count<10) {
+			System.out.println("Count is: "+count);
+			count++;
+		}
+
+	}
+
+}
