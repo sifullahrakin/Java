b72de915237156d734c031f3d7d75ef1564d3608
Rakin Mohammad Sifullah <rakin.sifullah@gmail.com>
2022-11-27 23:37:51 +0600

    Reduce

---
+++ Stream/Stream_Reduce.java
@@ -0,0 +1,12 @@
+package stream_package;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Stream_Reduce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
